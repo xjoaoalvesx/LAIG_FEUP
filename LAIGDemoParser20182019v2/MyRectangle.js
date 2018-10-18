02a4633c959ab79bcd42e9c1ba85fe43b74f3418
Tiago Cardoso <up201605762@fe.up.pt>
2018-10-18 17:58:56 +0100

    normals and textels [important]

--- LAIGDemoParser20182019v2/MyRectangle.js
+++ LAIGDemoParser20182019v2/MyRectangle.js
@@ -6,7 +6,7 @@
 
 class MyRectangle extends CGFobject
 {
-	constructor(scene, x1, x2, y1, y2) 
+	constructor(scene, x1, x2, y1, y2)
 	{
 		super(scene);
 		this.x1 = x1;
@@ -18,10 +18,10 @@ class MyRectangle extends CGFobject
 		this.maxS = x2-x1;
 		this.maxT = y2-y1;
 		this.initBuffers();
-		
+
 	};
 
-	initBuffers() 
+	initBuffers()
 	{
 		this.vertices = [
 				this.x1, this.y1, 0,
@@ -31,10 +31,10 @@ class MyRectangle extends CGFobject
 				];
 
 		this.indices = [
-				0, 1, 2, 
+				0, 1, 2,
 				3, 2, 1
 			];
-		
+
 		this.normals = [
 				0, 0, 1,
 				0, 0, 1,
@@ -44,23 +44,26 @@ class MyRectangle extends CGFobject
 
 		this.texCoords = [
 				this.minS, this.minT,
-				this.maxS, this.minT, 
+				this.maxS, this.minT,
 				this.minS, this.maxT,
-				this.maxS, this.maxT		
+				this.maxS, this.maxT
 		];
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	};
 
-	//funcao muda textcoord
-	//
-	//mS= maxS/length_s
-	//
-	//mT = maxT/length_t
-	//
-	// this.textCoords  = [...]
-	//
-	//
-	//this.updateTextCoordsGLBuffers();
+
+	updateTextCoords(length_s, length_t){
+		var mS = this.maxS/length_s;
+		var mT = this.maxT/length_t;
+		this.textCoords  = [
+			0,0,
+			mS,0,
+			0,mT,
+			mS,mT
+		];
+
+		this.updateTextCoordsGLBuffers();
+	}
 };
--- LAIGDemoParser20182019v2/Triangle.js
+++ LAIGDemoParser20182019v2/Triangle.js
@@ -6,7 +6,7 @@
 
 class Triangle extends CGFobject
 {
-	constructor(scene, x1, x2, x3, y1, y2, y3, z1, z2, z3) 
+	constructor(scene, x1, x2, x3, y1, y2, y3, z1, z2, z3)
 	{
 		super(scene);
 		this.x1 = x1;
@@ -23,10 +23,10 @@ class Triangle extends CGFobject
 		this.maxS = 1;
 		this.maxT = 1;
 		this.initBuffers();
-		
+
 	};
 
-	initBuffers() 
+	initBuffers()
 	{
 		this.vertices = [
 				this.x1, this.y1, this.z1,
@@ -35,20 +35,32 @@ class Triangle extends CGFobject
 				];
 
 		this.indices = [
-				0, 1, 2, 
+				0, 1, 2,
 			];
-		
+
+
+		let vec1 = [this.x2-this.x1, this.y2-this.y1, this.z2-this.z1];
+		let vec2 = [this.x3-this.x1, this.y3-this.y1, this.z3-this.z1];
+		let cross = vec3.fromValues(vec1[1]*vec2[2]-vec1[2]*vec1[2], vec1[2]*vec2[0]-vec1[0]*vec2[2], vec1[0]*vec2[1]-vec1[1]*vec2[0]);
+		vec3.normalize(cross, cross);
+
 		this.normals = [
-				0, 0, 1,
-				0, 0, 1,
-				0, 0, 1,
+				cross[0], cross[1], cross[2],
+				cross[0], cross[1], cross[2],
+				cross[0], cross[1], cross[2]
 		];
 
+		var d_vec1 = Math.sqrt(Math.pow(this.x0-this.x1, 2)+Math.pow(this.y0-this.y1, 2)+Math.pow(this.z0-this.z1, 2));
+		var d_vec2 = Math.sqrt(Math.pow(this.x0-this.x2, 2)+Math.pow(this.y0-this.y2, 2)+Math.pow(this.z0-this.z2, 2));
+		let cos = ((this.x2-this.x0)*(this.x1-this.x0)+(this.y2-this.y0)*(this.y1-this.y0)+(this.z2-this.z0)*(this.z1-this.z0))/(d_vec1*d_vec2);
+
+		var s_coord = cos*d_vec2;
+		var t_coord = -Math.sqrt(Math.pow(d_vec2,2)-Math.pow(s_coord,2));
+
 		this.texCoords = [
-				this.minS, this.minT,
-				this.maxS, this.minT, 
-				this.minS, this.maxT,
-				this.maxS, this.maxT		
+				0, 0,
+				d_vec1, 0,
+				s_coord,-t_coord,
 		];
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
