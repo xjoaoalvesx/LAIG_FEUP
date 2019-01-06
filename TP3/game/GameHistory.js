cf9d575255a7c33aa001a40f22c49588156120d2
Tiago Cardoso <up201605762@fe.up.pt>
2019-01-06 16:55:23 +0000

    Game history

--- TP3/game/GameHistory.js
+++ TP3/game/GameHistory.js
@@ -5,6 +5,30 @@ class GameHistory{
 
         this.scene = scene;
 
+        this.reset();
   }
 
+  reset(){
+    this.boards = new Array();
+    this.moves = new Array();
+  }
+
+  addmove(move, board){
+    this.moves.push(move);
+    this.boards.push(board);
+  }
+
+
+
+  diferencialConsecutiveBoards(board_previous, board_after){
+    for(var i = 0; i < board_previous.length; i++){
+      for(var j = 0; j < board_previous[i].length; j++){
+        if(board_after[i][j] == 0 || board_after[i][j] == board_previous[i][j])
+          continue;
+        else {
+          return [i,j];
+        }
+      }
+    }
+  }
 }
